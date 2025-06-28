471d963ed39e974e9c0a89f5b632eda3fb011780
Yukinari <yukinariito1207@gmail.com>
2025-06-28 15:01:12 +0900

    class choices_madeの使い方を表示

--- Main/choices_made.py
+++ Main/choices_made.py
@@ -1,4 +1,11 @@
 class choices_made():
+    """
+    choices_madeはインスタンス化して使う．
+    choices_made = choices_made()のようにする．
+    save_choiceを使うときは，
+    choices_made.save_choices(key, value)という形で使う．
+    selfは引数ではないので，特別何かを指定する必要はない．
+    """
     def __init__(self):
         self.choices_dict = {}
     
